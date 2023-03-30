ec5cc7942f49b033f36bbb22d44d7f37283af8e2
unknown <DanielBeltran2>
2023-03-29 20:37:57 -0700

    paso 1:crear base datos y leer todos

---
+++ Carros.sql
@@ -0,0 +1,5 @@
+Create table Carros(
+    id int not null,
+    marca nvarchar(20) not null,
+    nombre nvarchar(20) not null
+)
--- index.js
+++ index.js
@@ -4,6 +4,25 @@ const app = express()
 const port = 3000
 app.use(express.json())
 
+const mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "example",
+    database: "test1"
+});
+
+app.get('/todo',(req,res) =>{
+    con.query("select * from Carros",
+        function(err,result){
+            if(err){
+                throw err;
+            }
+            res.status(200).json(result);
+        });
+});
+
 app.listen(port, ()=>{
     console.log('Example app listening on port ${port}')
 })
